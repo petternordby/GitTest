a886ab10b4b6a5acb2214f9160c08ede9bde657f
petternordby <petteen@stud.ntnu.no>
2020-11-08 09:31:46 -0500

    testing file on  new branch

---
+++ GitTest.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "definitions.hpp"
+#include "CompGraph.hpp"
+#include "GenerateScenarios.hpp"
+
+#include "inputmodel.hpp"
+
+using namespace std;
+
+//The main file requires model file to take the adress input of the probability of each scenario
+//Return the number of scenarios
+int inputmodel(vector<double> &weights)
+{
+ 
+ return num_scenarios;   
+}
